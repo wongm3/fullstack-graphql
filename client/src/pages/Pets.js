22c68d5f20dde3170fe71ee5aacb1b47997d5f7f
Matt Wong <matthew067d.wong@libertymutual.com>
2020-03-25 17:20:23 -0400

    mutate data

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -16,19 +16,32 @@ const ALL_PETS = gql`
   }
 `;
 
+const CREATE_PET = gql`
+  mutation CreatePet($input: NewPetInput!) {
+    addPet(input: $input) {
+      id
+      type
+      name
+      img
+    }
+  }
+`;
+
 export default function Pets() {
   const [modal, setModal] = useState(false);
   const { data, loading, error } = useQuery(ALL_PETS);
+  const [createPet, newPet] = useMutation(CREATE_PET);
 
-  if (loading) {
+  if (loading || newPet.loading) {
     return <Loader />;
   }
 
-  if (error) {
+  if (error || newPet.error) {
     return <p>error</p>;
   }
 
   const onSubmit = input => {
+    createPet({ variables: { input } });
     setModal(false);
   };
 
